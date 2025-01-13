4f7b59af340813fd5758c52976df6bbcad522c31
Deependra-Das <iamdeep75@gmail.com>
2025-01-13 17:51:20 +0530

    Implemented Singleton for MapService

--- Assets/Scripts/Map/MapService.cs
+++ Assets/Scripts/Map/MapService.cs
@@ -16,6 +16,22 @@ namespace ServiceLocator.Map
         private MapData currentMapData;
         private SpriteRenderer tileOverlay;
 
+        private static MapService instance;
+        public static MapService Instance { get { return instance; } }
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(this.gameObject);
+                Debug.Log("Additional instance of MapService destroyed.");
+            }
+        }
+
         private void Start()
         {
             SubscribeToEvents();
--- Assets/Scripts/Player/PlayerService.cs
+++ Assets/Scripts/Player/PlayerService.cs
@@ -10,7 +10,6 @@ namespace ServiceLocator.Player
     public class PlayerService : MonoBehaviour
     {
         [SerializeField] private UIService uiService;
-        [SerializeField] private MapService mapService;
         [SerializeField] private SoundService soundService;
 
 
@@ -100,7 +99,7 @@ namespace ServiceLocator.Player
             if (monkeyCost > Money)
                 return;
 
-            mapService.ValidateSpawnPosition(dropPosition);
+            MapService.Instance.ValidateSpawnPosition(dropPosition);
         }
 
         public void TrySpawningMonkey(MonkeyType monkeyType, int monkeyCost, Vector3 dropPosition)
@@ -108,7 +107,7 @@ namespace ServiceLocator.Player
             if (monkeyCost > Money)
                 return;
 
-            if (mapService.TryGetMonkeySpawnPosition(dropPosition, out Vector3 spawnPosition))
+            if (MapService.Instance.TryGetMonkeySpawnPosition(dropPosition, out Vector3 spawnPosition))
             {
                 SpawnMonkey(monkeyType, spawnPosition);
                 soundService.PlaySoundEffects(SoundType.SpawnMonkey);
