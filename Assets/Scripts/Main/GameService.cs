e1a53d585d80ebef9d57ca88e2bb3c6f7070f0af
Deependra-Das <iamdeep75@gmail.com>
2025-01-15 01:23:20 +0530

    Implemented SL Changes for UIService

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,5 +1,6 @@
 using ServiceLocator.Player;
 using ServiceLocator.Sound;
+using ServiceLocator.UI;
 using ServiceLocator.Utilities;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,6 +12,9 @@ public class GameService : GenericMonoSingleton<GameService>
     [SerializeField] private SoundScriptableObject soundScriptableObject;
     [SerializeField] private AudioSource audioEffects;
     [SerializeField] private AudioSource backgroundMusic;
+
+    [SerializeField] private UIService uiService;
+    public UIService UIService => uiService;
     public PlayerService playerService {  get; private set; }
     public SoundService soundService { get; private set; }
 
