807f4484a451b9e52e1835ea836c75ff995ec8f2
Deependra-Das <iamdeep75@gmail.com>
2025-01-17 00:41:22 +0530

    Bug fixes & Code Cleanup

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -12,11 +12,11 @@ namespace ServiceLocator.Main
     public class GameService : MonoBehaviour
     {
         // Services:
-        public EventService EventService { get; private set; }
-        public MapService MapService { get; private set; }
-        public WaveService WaveService { get; private set; }
-        public SoundService SoundService { get; private set; }
-        public PlayerService PlayerService { get; private set; }
+        public EventService eventService;
+        public MapService mapService;
+        public WaveService waveService;
+        public SoundService soundService;
+        public PlayerService playerService;
 
         [SerializeField] private UIService uiService;
         public UIService UIService => uiService;
@@ -40,24 +40,24 @@ namespace ServiceLocator.Main
 
         private void createServices()
         {
-            EventService = new EventService();
-            MapService = new MapService(mapScriptableObject);
-            WaveService = new WaveService(waveScriptableObject);
-            SoundService = new SoundService(soundScriptableObject, SFXSource, BGSource);
-            PlayerService = new PlayerService(playerScriptableObject);
+            eventService = new EventService();
+            mapService = new MapService(mapScriptableObject);
+            waveService = new WaveService(waveScriptableObject);
+            soundService = new SoundService(soundScriptableObject, SFXSource, BGSource);
+            playerService = new PlayerService(playerScriptableObject);
         }
 
         private void InjectDependencies()
         {
-            UIService.Init(WaveService, EventService, PlayerService);
-            PlayerService.Init(UIService, MapService, SoundService);
-            WaveService.Init(UIService, MapService, SoundService, EventService, PlayerService);
-            MapService.Init(EventService);
+            UIService.Init(waveService, eventService, playerService);
+            playerService.Init(UIService, mapService, soundService);
+            waveService.Init(UIService, mapService, soundService, eventService, playerService);
+            mapService.Init(eventService);
         }
 
         private void Update()
         {
-            PlayerService.Update();
+            playerService.Update();
         }
     }
 }
